cdd2b8e85b19f4005ee05c5bba84e0f0937ab5b6
Seiya Kobayashi <seiyak@bu.edu>
2018-05-09 04:25:18 -0400

    Bug fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,15 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
     @user = @post.user
-    # Search GroupId model, given @post.id
-    @tags = GroupId.where(post_id: @post.id)
-    # Array to store tags
-    @arr = Array.new
-    @tags.each do |tag|
-      @arr.push(Tag.find_by(id: tag.tag_id))
+    @tags = GroupId.where(post_id: @post.id)     # Search GroupId model, given @post.id
+    @tags_new = Array.new     # Array to store tags
+    @tag_bit = 0     # Bit that represents whether post have any tags
+
+    if @tags.length != 0
+      @tag_bit = 1
+      @tags.each do |tag|
+        @tags_new.push(Tag.find_by(id: tag.tag_id))
+      end
     end
   end
 
@@ -26,9 +29,10 @@ class PostsController < ApplicationController
     @post = Post.new(content: params[:content])
   end
 
-  # create is called from "new.html.erb"
+  # create is called from "posts/new.html.erb"
   def create
     @post = Post.new(content: params[:content], user_id: @current_user.id)
+
     if @post.save
       flash[:notice] = "Your post has been added."
       redirect_to("/posts/index")
@@ -42,10 +46,11 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
   end
 
-  # update is called from "edit.html.erb"
+  # update is called from "posts/edit.html.erb"
   def update
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
+
     if @post.save
       flash[:notice] = "Your post has been updated."
       redirect_to("/posts/index")
@@ -54,9 +59,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # delete is called from "show.html.erb"
+  # delete is called from "posts/show.html.erb"
   def delete
     @post = Post.find_by(id: params[:id])
+
     @post.destroy
     flash[:notice] = "Your post has been deleted."
     redirect_to("/posts/index")
@@ -65,6 +71,7 @@ class PostsController < ApplicationController
   # Check if the post that current_user is about to edit/delete is his/her own
   def ensure_correct_user
     @post = Post.find_by(id: params[:id])
+
     if @post.user_id != @current_user.id
       flash[:notice] = "You do not have authorization. Redirected to your personal page."
       redirect_to("/users/#{@current_user.id}")
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,46 +3,37 @@ class TagsController < ApplicationController
   # List all tags in descending order
   def index
     @tags = Tag.all.order(created_at: :asc)
-    # Array to store group_ids
-    @arr = Array.new
-    # Array to store tags (no same labels)
-    @tags_new = Array.new
+    @tags_new = Array.new     # Array to store tags (no same labels)
 
     @tags.each do |tag|
-      # Valid bit to detemine if it has the same label as other tags
-      @valid = 1
-      # If it's the very first tag
-      if @arr.length == 0
+      @same = 0     # Bit that represents whether tag has the same label as other tags
+      if @tags_new.length == 0
         @tags_new.push(tag)
-        @arr.push(tag.group_id)
-      # Otherwise
       else
-        @arr.each do |id|
-          # If the same group_id is found, set valid bit to 0 and jump to next tag
-          if tag.group_id == id
-            @valid = 0
+        @tags_new.each do |tag_new|
+          # If the same group_id is found, set the bit and jump to next tag
+          if tag.group_id == tag_new.group_id
+            @same = 1
             break
           end
         end
-        if @valid == 0
+        if @same == 1
           next
         else
           @tags_new.push(tag)
-          @arr.push(tag.group_id)
         end
       end
     end
   end
 
-  # Show tag details
+  # Show tag details (list of posts to which the tag is attached)
   def show
     @tag = Tag.find_by(id: params[:id])
-    # Search GroupId model, given @tag.group_id
-    @post_new = GroupId.where(tag_group_id: @tag.group_id)
-    # Array to store posts
-    @arr = Array.new
-    @post_new.each do |post|
-      @arr.push(Post.find_by(id: post.post_id))
+    @posts = GroupId.where(tag_group_id: @tag.group_id)     # Search GroupId model, given @tag.group_id
+    @posts_new = Array.new     # Array to store posts
+
+    @posts.each do |post|
+      @posts_new.push(Post.find_by(id: post.post_id))
     end
   end
 
@@ -52,61 +43,66 @@ class TagsController < ApplicationController
     @tag = Tag.new(label: params[:label])
   end
 
+  # create is called from "tags/new.html.erb"
   def create
     @post = Post.find_by(id: params[:id])
     @tag = Tag.new(label: params[:label], post_id: params[:id])
     @tags = Tag.all.order(created_at: :desc)
-    # If it's the very first tag you created
+    @changed = 0     # Bit that represents whether group_id has been changed
+
+    # If it's the very first tag you create
     if @tags.length == 0
-      # If invalid entry
+      # If invalid (empty) entry
       if !@tag.save
         flash[:notice] = "Please enter a valid tag name."
         redirect_to("/posts/#{@post.id}/tags")
         return
-      # If valid entry
       else
         @tag.group_id = @tag.id
         @tag.save
-        # Function as an array to store multiple group_ids / tag_ids / post_ids for one post
+        # GroupId is a model that stores tag_group_ids, post_ids, and tag_ids
         @group_id = GroupId.new(tag_group_id: @tag.group_id, post_id: @post.id, tag_id: @tag.id)
         @group_id.save
         flash[:notice] = "A new tag has been added."
         redirect_to("/tags/index")
         return
       end
-    # If invalid entry
-    elsif !@tag.save
-      flash[:notice] = "Please enter a valid tag name."
-      redirect_to("/posts/#{@post.id}/tags")
-      return
-    # If valid entry
+    # After creating the first tag
     else
-      @tags.each do |tag|
-        # If same label already exists, add post to that tag
-        if @tag.label == tag.label
-          # Avoid attaching same tag to the same post
-          if @tag.post_id == tag.post_id
+      # If invalid (empty) entry
+      if !@tag.save
+        flash[:notice] = "Please enter a valid tag name."
+        redirect_to("/posts/#{@post.id}/tags")
+        return
+      else
+        @tags.each do |tag|
+          # If same label already exists, add post to that tag (Avoid attaching same tag to the same post)
+          if @tag.label.downcase == tag.label.downcase && @tag.post_id == tag.post_id
+            @tag.destroy
             flash[:notice] = "Same tag cannot be attached."
             redirect_to("/posts/#{@post.id}/tags")
             return
-          else
+          elsif @tag.label.downcase == tag.label.downcase && @tag.post_id != tag.post_id
             @tag.group_id = tag.group_id
-            @tag.save
+            @changed = 1
+            next
           end
-          @group_id = GroupId.new(tag_group_id: @tag.group_id, post_id: @post.id, tag_id: @tag.id)
-          @group_id.save
-          flash[:notice] = "This post has been added to existing label: '#{@tag.label}'."
-          redirect_to("/tags/index")
-          return
         end
+        if @changed == 0
+          @tag.group_id = @tag.id
+        end
+        @tag.save
+        @group_id = GroupId.new(tag_group_id: @tag.group_id, post_id: @post.id, tag_id: @tag.id)
+        @group_id.save
+        if @changed == 1
+          flash[:notice] = "This post has been added to the existing label: '#{@tag.label}'."
+        else
+          flash[:notice] = "A new tag has been added."
+        end
+        redirect_to("/tags/index")
+        return
       end
-      @tag.group_id = @tag.id
-      @tag.save
-      @group_id = GroupId.new(tag_group_id: @tag.group_id, post_id: @post.id, tag_id: @tag.id)
-      @group_id.save
-      flash[:notice] = "A new tag has been added."
-      redirect_to("/tags/index")
-      return
     end
   end
+  
 end
