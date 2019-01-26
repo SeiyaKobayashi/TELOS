0e25c16748e532ff10314ac7a4ef6a5f33f9b1a0
Seiya Kobayashi <seiyak@bu.edu>
2019-01-26 16:29:00 -0500

    Resolve migration errors.

--- db/migrate/20190126090910_add_encrypted_password_to_user.rb
+++ db/migrate/20190126090910_add_encrypted_password_to_user.rb
@@ -1,4 +1,4 @@
-class AddEncryptedPasswordColumnsToUser < ActiveRecord::Migration[5.2]
+class AddEncryptedPasswordToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :encrypted_password, :string
     add_column :users, :encrypted_password_iv, :string
